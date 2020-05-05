20549bc543cd683dfda61e624338522e198157e8
KimHuicheol <kim.huicheol.85@gmail.com>
2020-05-05 01:55:38 +0000

    [FIN]_6.1_Sessionsコントローラを作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  #ログイン機能
+  get    '/login' , to: 'session#new'
+  post   '/login' , to: 'session#create'
+  delete '/logout', to: 'session#destory'
+  
   resources :users
 end
