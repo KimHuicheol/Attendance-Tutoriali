1e87803bad76df5469cc6d52a958f8f462359850
KimHuicheol <kim.huicheol.85@gmail.com>
2020-05-06 08:24:53 +0000

    Finish full-fledged-login_[7.3]_チェックボックスを追加しよう_第7章完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,7 @@ module SessionsHelper
   
   #セッションと@current_userを破棄します
   def log_out
-    foreget(current_user)
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -31,7 +31,7 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookiees.signed[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && User.authenticated?(cookies[:remember_token])
         log_in user
